64d340ae264574bbe32ddfe480dd7fbffab63bfe
Skyisforever <46169434+Skyisforever@users.noreply.github.com>
2019-01-18 00:35:38 -0500

    Add files via upload

---
+++ Attack.java
@@ -0,0 +1,13 @@
+
+public class Attack {
+double basedamage;
+String name;
+String type;
+Status status;
+public Attack(double basedamage, String name, String type, Status status) {
+	this.basedamage=basedamage;
+	this.name=name;
+	this.type=type;
+	this.status=status;
+}
+}
