1656b109463e91437dab223e9ef81382aee47751
Peter Ho <ostentatiousitus@gmail.com>
2019-01-21 18:20:40 -0500

    fixed typewriter race condition (isTYping)

--- src/Typewriter.java
+++ src/Typewriter.java
@@ -1,62 +1,71 @@
-public class Typewriter implements Runnable {
-	private int x = 0;
-	private String text;
-	private boolean isTyping;
-	private int speed;
-	private boolean go;
-	private Hud hud;
+public class Typewriter implements Runnable
+{
+    private int     x = 0;
+    private String  text;
+    private boolean isTyping;
+    private int     speed;
+    private boolean go;
+    private Hud     hud;
 
-	public void run() {
-		System.out.println("Entered thread.");
-		while (true) {
-			while (!go) {
-				try {
-					Thread.sleep(speed);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			go = false;
-			while (true) {
-				if (x < text.length()) {
-					++x;
-					isTyping = true;
-				} else {
-					isTyping = false;
-					break;
-				}
-				hud.text = text.substring(0, x);
-				try {
-					Thread.sleep(this.speed);
-				} catch (Exception e) {
-				}
-			}
-		}
-	}
+    public void run()
+    {
+        System.out.println("Entered thread.");
+        while (true) {
+            while (go) {
+                if (x < text.length()) {
+                    ++x;
+                    isTyping = true;
+                } else {
+                    isTyping = false;
+                    go = false;
+                    break;
+                }
+                hud.text = text.substring(0, x);
+                
+                try {
+                    Thread.sleep(speed);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
-	public void next() {
-		go = true;
-	}
+            try {
+                Thread.sleep(speed);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public Typewriter(int speed, Hud hud) {
-		this.hud = hud;
-		x = 0;
-		this.speed = speed;
-		isTyping = false;
-		go = false;
-	}
+    public void next()
+    {
+        go = true;
+        isTyping = true;
+    }
 
-	public void set(String text) {
-		this.text = text;
-		this.x = 0;
-	}
+    public Typewriter(int speed, Hud hud)
+    {
+        this.hud = hud;
+        x = 0;
+        this.speed = speed;
+        isTyping = false;
+        go = false;
+    }
 
-	public void end() {
-		this.x = text.length() - 1;
-	}
+    public void set(String text)
+    {
+        this.text = text;
+        this.x = 0;
+    }
 
-	public boolean isTyping() {
-		return isTyping;
-	}
+    public void end()
+    {
+        this.x = text.length() - 1;
+    }
+
+    public boolean isTyping()
+    {
+        return isTyping;
+    }
 
 }
