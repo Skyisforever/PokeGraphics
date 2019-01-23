a8d59188db7b6bb7df4676c51c991012a9816c97
Skyisforever <46169434+Skyisforever@users.noreply.github.com>
2019-01-22 23:56:41 -0500

    Add files via upload
    added some sheit in pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -7,6 +9,7 @@ import javax.imageio.ImageIO;
 
 public class Pokemon {
 	BufferedImage pokemonimage=null;
+	AffineTransformOp op;
 	String name;
 	ArrayList<String> type;
 	ArrayList<Attack> skills;
@@ -37,41 +40,42 @@ public class Pokemon {
 				e.printStackTrace();
 			}
 		}
-	 else if (name.equals("bulbasaur")) {
-			try {
-				pokemonimage=ImageIO.read(new File("bulbasaur.png"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	 else if (name.equals("jigglypuff")) {
-			try {
-				pokemonimage=ImageIO.read(new File("jigglypuff.png"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	 else if (name.equals("greninja")) {
-			try {
-				pokemonimage=ImageIO.read(new File("greninja.png"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	 else if (name.equals("psyduck")) {
-			try {
-				pokemonimage=ImageIO.read(new File("psyduck.png"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+//	 else if (name.equals("bulbasaur")) {
+//			try {
+//				pokemonimage=ImageIO.read(new File("bulbasaur.png"));
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	 else if (name.equals("jigglypuff")) {
+//			try {
+//				pokemonimage=ImageIO.read(new File("jigglypuff.png"));
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	 else if (name.equals("greninja")) {
+//			try {
+//				pokemonimage=ImageIO.read(new File("greninja.png"));
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	 else if (name.equals("psyduck")) {
+//			try {
+//				pokemonimage=ImageIO.read(new File("psyduck.png"));
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
 	
 	}
 	public void physics() {
 		
 	}
-	public static void draw(Graphics g) {
-		
+	public void draw(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(op.filter(pokemonimage, null), 400, 600, null);
 	}
 	
 }
