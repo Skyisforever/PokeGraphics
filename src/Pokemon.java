bc74f47b5ad561fed5b42fc738bcd37e60adc009
Skyisforever <46169434+Skyisforever@users.noreply.github.com>
2019-01-22 23:51:31 -0500

    Add files via upload
    added some draw classes

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,6 +1,12 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
 public class Pokemon {
+	BufferedImage pokemonimage=null;
 	String name;
 	ArrayList<String> type;
 	ArrayList<Attack> skills;
@@ -11,7 +17,6 @@ public class Pokemon {
 	double currenthealth;
 	ArrayList<Status> statuses;
 	Attack currentattack;
-
 	public Pokemon(String name, ArrayList<String> type, double health, double attack, double defense, double speed,
 			ArrayList<Attack> skills, ArrayList<Status> statuses) {
 		this.name = name;
@@ -24,4 +29,49 @@ public class Pokemon {
 		this.statuses = statuses;
 		currenthealth = health;
 	}
+	public void define() {
+	 if (name.equals("pikachu")) {
+			try {
+				pokemonimage = ImageIO.read(new File("pika.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	 else if (name.equals("bulbasaur")) {
+			try {
+				pokemonimage=ImageIO.read(new File("bulbasaur.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	 else if (name.equals("jigglypuff")) {
+			try {
+				pokemonimage=ImageIO.read(new File("jigglypuff.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	 else if (name.equals("greninja")) {
+			try {
+				pokemonimage=ImageIO.read(new File("greninja.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	 else if (name.equals("psyduck")) {
+			try {
+				pokemonimage=ImageIO.read(new File("psyduck.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	
+	}
+	public void physics() {
+		
+	}
+	public static void draw(Graphics g) {
+		
+	}
+	
 }
