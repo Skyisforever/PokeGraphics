2c05c11a234c00b373ae17f1db2507cc70cabc09
Peter Ho <ostentatiousitus@gmail.com>
2019-01-23 01:07:45 -0500

    change animation logic

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class Pokemon {
-	BufferedImage pokemonimage=null;
+	BufferedImage pokemonimage = null;
 	AffineTransformOp op;
 	String name;
 	ArrayList<String> type;
@@ -22,10 +22,10 @@ public class Pokemon {
 	Attack currentattack;
 	String animationType;
 	Player player;
-	
+
 	float x, y;
 	boolean opponent = false;
-	
+
 	public Pokemon(String name, ArrayList<String> type, double health, double attack, double defense, double speed,
 			ArrayList<Attack> skills, ArrayList<Status> statuses, Player player) {
 		this.name = name;
@@ -40,108 +40,120 @@ public class Pokemon {
 		currenthealth = health;
 		define();
 	}
+
 	public void define() {
-	    if (player.name.equals("opponent")) {
-	        x = 450;
-	        y = 45;
-	        opponent = true;
-	    } else {
-    	    x = 20;
-    	    y = 300;
-	    }
-	    animationType = "";
-	    name = "Pikachu";
-	    //if (name.equals("Pikachu")) {
-			try {
-				pokemonimage = ImageIO.read(new File("pika.png"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		//}
-//	 else if (name.equals("bulbasaur")) {
-//			try {
-//				pokemonimage=ImageIO.read(new File("bulbasaur.png"));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	 else if (name.equals("jigglypuff")) {
-//			try {
-//				pokemonimage=ImageIO.read(new File("jigglypuff.png"));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	 else if (name.equals("greninja")) {
-//			try {
-//				pokemonimage=ImageIO.read(new File("greninja.png"));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	 else if (name.equals("psyduck")) {
-//			try {
-//				pokemonimage=ImageIO.read(new File("psyduck.png"));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-	
+		if (player.name.equals("opponent")) {
+			x = 450;
+			y = 45;
+			opponent = true;
+		} else {
+			x = 20;
+			y = 300;
+		}
+		animationType = "";
+		name = "Pikachu";
+		// if (name.equals("Pikachu")) {
+		try {
+			pokemonimage = ImageIO.read(new File("pika.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// }
+		// else if (name.equals("bulbasaur")) {
+		// try {
+		// pokemonimage=ImageIO.read(new File("bulbasaur.png"));
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// else if (name.equals("jigglypuff")) {
+		// try {
+		// pokemonimage=ImageIO.read(new File("jigglypuff.png"));
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// else if (name.equals("greninja")) {
+		// try {
+		// pokemonimage=ImageIO.read(new File("greninja.png"));
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// else if (name.equals("psyduck")) {
+		// try {
+		// pokemonimage=ImageIO.read(new File("psyduck.png"));
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }
+
 	}
-	
-	
+
 	public void attack() {
-	    switch(name) {
-	    case "Pikachu":
-	        animationType = "pika";
-	        break;
-	    default:
-	        animationType = "pika";
-	        break;
-	    }
+		animationType = "attack";
 	}
-	
+
+	public void faint() {
+		animationType = "faint";
+	}
+
 	public boolean animationPlaying() {
-	    return animationType.equals("");
+		return animationType.equals("");
 	}
-	
-	
+
 	int frame_counter = 0;
 	int dx = 0;
 	int dy = 0;
+
 	public void physics() {
-	    if (!animationType.equals("")) {
-	        switch(animationType) {
-	        case "pika":
-	            if (frame_counter >= 30) {
-	                animationType = "";
-	                frame_counter = 0;
-	                break;
-	            } else if (frame_counter < 15) {
-	                dx = 1;
-	                dy = -1;
-	            } else if (frame_counter >= 15){
-	                dx = -1;
-	                dy = 1;
-	            }
-	            frame_counter++;
-	            break;
-	        }
-	        
-	        if (opponent) {
-	            dx *= -1;
-	            dy *= -1;
-	        }
-	        
-	        x += dx;
-	        y += dy;
-	    }
-		
+		if (!animationType.equals("")) {
+			switch (animationType) {
+			case "attack":
+				switch (currentattack.name) {
+				case "QuickAttack":
+				case "Nuzzle":
+					if (frame_counter >= 30) {
+						animationType = "";
+						frame_counter = 0;
+						break;
+					} else if (frame_counter < 15) {
+						dx = 1;
+						dy = -1;
+					} else if (frame_counter >= 15) {
+						dx = -1;
+						dy = 1;
+					}
+					break;
+				case "Thunder":
+					// do some other animation
+					break;
+				default:
+					animationType = "";
+				} // end switch(currentattack.name)
+				break;
+
+			case "faint":
+				// faint animation here
+			}
+
+			frame_counter++;
+
+			if (opponent) {
+				dx *= -1;
+				dy *= -1;
+			}
+
+			x += dx;
+			y += dy;
+		}
+
 	}
+
 	public void draw(Graphics g) {
-		//Graphics2D g2d = (Graphics2D) g;
-		//g2d.drawImage(op.filter(pokemonimage, null), 400, 600, null);
-	    g.drawImage(pokemonimage, (int)x, (int)y, null);
+		// Graphics2D g2d = (Graphics2D) g;
+		// g2d.drawImage(op.filter(pokemonimage, null), 400, 600, null);
+		g.drawImage(pokemonimage, (int) x, (int) y, null);
 	}
-	
+
 }
